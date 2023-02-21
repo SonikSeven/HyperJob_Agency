9fecaa97bcdd966522cdd113ff967be71e33066d
Mikhail Semenov <misha368@yahoo.com>
2023-02-21 13:15:50 +0300

    Initial commit

---
+++ menu/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, redirect_to_login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
+
+
+class Main(View):
+    menu_links = {"Login page": reverse_lazy("login"),
+                  "Logout page": reverse_lazy("logout"),
+                  "Sign-up": reverse_lazy("signup"),
+                  "Vacancy": reverse_lazy("vacancies"),
+                  "Resume": reverse_lazy("resumes"),
+                  "Personal profile": reverse_lazy("home")}
+
+    def get(self, request):
+        return render(request, "menu/main.html", context={"menu_links": self.menu_links})
+
+
+class HyperSignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "menu/signup.html"
+
+
+class HyperLoginView(LoginView):
+    redirect_authenticated_user = True
+    template_name = "menu/login.html"
+
+
+class HomeView(CreateView):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return HttpResponseRedirect(reverse("new_vacancy"))
+        elif request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("new_resume"))
+        else:
+            return redirect_to_login(reverse("home"))
---
+++ resume/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+from . import views
+
+urlpatterns = [
+    re_path("resumes/?", views.ResumesView.as_view(), name="resumes"),
+    re_path("resume/new/?", views.NewResumeView.as_view(), name="new_resume"),
+]
